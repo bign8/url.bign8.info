6e46775c050256b802872abe827a5b0c61b3a4b5
Nathan Woods <big.nate.w@gmail.com>
2014-05-10 16:06:12 -0600

    Saving SURLs

--- index.php
+++ index.php
@@ -3,39 +3,33 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-function convert($dividend) {
-	// http://www.ietf.org/rfc/rfc3986.txt  2.3
-	$alphalbet = array(
-		'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-		'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-		'0','1','2','3','4','5','6','7','8','9',
-		'-','.','_','~'//,'(',')','*','&','$','!','@'
-	);
-
-	$hash = '';
-	$size = count( $alphalbet );
-
-	while ( $dividend > 0 ) {
-		$hash .= $alphalbet[ $dividend % $size ];
-		$dividend = floor( $dividend / $size );
+try {
+	if (isset($_REQUEST['act']) && $_REQUEST['act'] == 'add') {
+		if (!filter_var($_REQUEST['url'], FILTER_VALIDATE_URL)) throw new Exception("Invalid URL format", 1);
+		
+		$db = new PDO('sqlite:db.db');
+		if ($db->prepare("INSERT INTO url (url) VALUES (?);")->execute(array($_REQUEST['url']))) { // Add new hash
+			$urlID = $db->lastInsertID();
+			$hash = base_convert($urlID, 10, 36);
+			if (!$db->prepare("UPDATE url SET hash=? WHERE urlID=?;")->execute(array($hash, $urlID))) throw new Exception(print_r($db->errorInfo()[2], true), 1);
+		} else { // url already exists
+			$sth = $db->prepare("SELECT hash FROM url WHERE url=? LIMIT 1;");
+			if (!$sth->execute(array($_REQUEST['url']))) throw new Exception(print_r($db->errorInfo()[2], true), 1);
+			$hash = $sth->fetchColumn();
+		}
+		
+		if ($hash === false) throw new Exception("Something went very wrong!", 1);
+		
+		// Redirect to preview page
+		// exit(header('Location: /url/' . $hash));
+		echo $hash;
 	}
-
-	return $hash;
-}
-
-if (isset($_REQUEST['act'])) switch ($_REQUEST['act']) {
-	case 'add':
-		echo 'adding to db';
-		echo '"' . convert($_REQUEST['url']) . '"';
-		break;
+} catch (Exception $e) {
+	// TODO: pretty print errors
+	throw $e;
 }
 
 ?>
-
-<!-- <pre> -->
-<!--<?php print_r($_REQUEST); ?>-->
-<!-- </pre> -->
-
 <!doctype html>
 <html lang="en">
 	<head>
@@ -44,10 +38,7 @@ if (isset($_REQUEST['act'])) switch ($_REQUEST['act']) {
 		<title>SURL</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
-		<!-- // <script language="javascript" src="library/all.js"></script> -->
-		<!-- <link rel="stylesheet" type="text/css" href="library/all.css" /> -->
 	</head>
 	<body>
 		<div class="container">
@@ -59,22 +50,26 @@ if (isset($_REQUEST['act'])) switch ($_REQUEST['act']) {
 
 			<p>SURL combines legendary URL shrinking advances with stunning analytics.  When these two forces combine, you get a top notch web experience.</p>
 
-			<div class="col-lg-6 col-lg-offset-3 col-md-10 col-md-offset-1">
-				<form method="POST" name="add" class="well">
-					<fieldset>
-						<legend>SURL Submission</legend>
-						<div class="form-group" style="margin-bottom:0">
-							<div class="input-group">
-								<label for="url" class="input-group-addon">URL: </label>
-								<input id="url" name="title" placeholder="http://" type="text" size="50" class="form-control">
-								<span class="input-group-btn">
-									<button type="submit" name="act" value="add" class="btn btn-default">Slim this URL</button>
-								</span>
+			<div class="row">
+				<div class="col-lg-6 col-lg-offset-3 col-md-10 col-md-offset-1">
+					<form method="POST" name="add" class="well">
+						<fieldset>
+							<legend>SURL Submission</legend>
+							<div class="form-group" style="margin-bottom:0">
+								<div class="input-group">
+									<label for="url" class="input-group-addon">URL: </label>
+									<input id="url" name="title" placeholder="http://" type="text" size="50" class="form-control">
+									<span class="input-group-btn">
+										<button type="submit" name="act" value="add" class="btn btn-default">Slim this URL</button>
+									</span>
+								</div>
 							</div>
-						</div>
-					</fieldset>
-				</form>
+						</fieldset>
+					</form>
+				</div>
 			</div>
+
+			<!-- <pre><?php print_r($_REQUEST); ?></pre> -->
 		</div>
 	</body>
 </html>
